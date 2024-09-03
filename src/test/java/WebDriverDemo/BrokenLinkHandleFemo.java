96f118a8d0f839d62e50753c53d2a9a42b5e2c5a
emran71 <ekhan4475@gmail.com>
2024-09-03 18:13:10 +0600

    Addblocker,BrokenLink,Handless Handle

---
+++ src/test/java/WebDriverDemo/BrokenLinkHandleFemo.java
@@ -0,0 +1,55 @@
+package WebDriverDemo;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrokenLinkHandleFemo {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.navigate().to("http://www.deadlinkcity.com/");
+	    driver.manage().window().maximize();
+	    int count_broken_link = 0;
+	    int count_valid_link = 0;
+	    List <WebElement> UrlList = driver.findElements(By.tagName("a"));
+	    System.out.println("Total URL: "+UrlList.size());
+	    for(WebElement list:UrlList ) {
+	    	
+	    	String hreflists = list.getAttribute("href");
+	    	if(hreflists == null || hreflists.isEmpty() )
+	    	{
+	    		System.out.println("This is not possible to count because it is empty");
+	    		continue;
+	    	}
+	    	try{
+	    	
+	    		URL str_url = new URL(hreflists);
+	    	
+	    	   HttpURLConnection conn = (HttpURLConnection) str_url.openConnection();
+	    	   conn.connect();
+	    	if(conn.getResponseCode()>= 400) {
+	    		System.out.println(hreflists+" Broken Link");
+	    		count_broken_link++;
+	    	}
+	    	else {
+	    		System.out.println(hreflists+" Not Broken Link");
+	    		count_valid_link++;
+	    	}
+	    }
+	    	catch(Exception e) {}
+	    	
+	    }
+	    System.out.println("Total Broken link : "+count_broken_link );
+	    System.out.println("Total Valid link : "+count_valid_link );
+	    
+	}
+
+}
--- src/test/java/WebDriverDemo/HandleAddBlocker.java
+++ src/test/java/WebDriverDemo/HandleAddBlocker.java
@@ -1,23 +1,23 @@
 package WebDriverDemo;
 
+import java.io.File;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class HandleSslDemu {
+public class HandleAddBlocker {
 
 	public static void main(String[] args) {
-		
 		ChromeOptions option = new ChromeOptions();
-		option.setAcceptInsecureCerts(true);
+		File file =  new File("K:\\CRX file\\AdBlock-—-block-ads-across-the-web-Chrome-Web-Store.crx");
+		option.addExtensions(file);
 		
 		WebDriver driver = new ChromeDriver(option);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.navigate().to("https://expired.badssl.com/");
+		driver.navigate().to("https://stackoverflow.com/questions/7890160/embed-website-into-my-site");
 	    driver.manage().window().maximize();
-       System.out.println(driver.getTitle());
 	}
 
 }
